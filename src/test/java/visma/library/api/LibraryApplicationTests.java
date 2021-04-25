5cfc6e2c01c88d1e49f6a6564509cbd34a0c0466
donatas <d.kadakauskis@gmail.com>
2021-04-25 16:34:15 +0300

    Initial commit

---
+++ src/test/java/visma/library/api/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package visma.library.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
